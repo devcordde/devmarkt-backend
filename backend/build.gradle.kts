6468fad6d54df2d06e3a936b5910697d5ed63357
nick hensel <nickhensel25@icloud.com>
2021-11-22 22:17:54 +0100

    Add swagger api specification generation

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,11 +20,13 @@ micronaut {
 }
 
 dependencies {
+    annotationProcessor("io.micronaut.openapi:micronaut-openapi:3.2.0")
     annotationProcessor("io.micronaut:micronaut-http-validation")
     annotationProcessor("io.micronaut:micronaut-inject-java:3.1.4")
     implementation("io.micronaut:micronaut-http-client")
     implementation("io.micronaut:micronaut-runtime")
     implementation("javax.annotation:javax.annotation-api")
+    implementation("io.swagger.core.v3:swagger-annotations")
     runtimeOnly("ch.qos.logback:logback-classic")
 
     implementation("org.mongojack:mongojack:4.3.0")
