272c265f09a7cb4f63347d005f778d82fb6dd0bb
Nick Hensel <nickhensel25@icloud.com>
2022-03-06 00:15:33 +0100

    Add/Update (missing) copyright

--- backend/src/main/java/club/devcord/devmarkt/database/template/TemplateDAO.java
+++ backend/src/main/java/club/devcord/devmarkt/database/template/TemplateDAO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Contributors to the Devmarkt-Backend project
+ * Copyright 2022 Contributors to the Devmarkt-Backend project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
