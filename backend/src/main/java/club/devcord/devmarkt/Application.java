847ae8422521be6b2e38ec0dc0191dee3f534a5b
Nick Hensel <nickhensel25@icloud.com>
2022-03-12 23:07:00 +0100

    Prepare project

--- backend/src/main/java/club/devcord/devmarkt/Application.java
+++ backend/src/main/java/club/devcord/devmarkt/Application.java
@@ -17,18 +17,7 @@
 package club.devcord.devmarkt;
 
 import io.micronaut.runtime.Micronaut;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Devmarkt-Backend",
-        version = "0.1",
-        description = "Rest API Specification for the Devmarkt-Backend",
-        license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")
-    )
-)
 public class Application {
 
   public static void main(String[] args) {
