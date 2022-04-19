fdbc997c0446a25f5d4847f658adc14cac709eaa
Nick Hensel <nickhensel25@icloud.com>
2022-04-20 00:46:09 +0200

    Reimplement schema permission generation

--- backend/src/main/java/club/devcord/devmarkt/auth/PermissionUpdater.java
+++ backend/src/main/java/club/devcord/devmarkt/auth/PermissionUpdater.java
@@ -16,26 +16,11 @@
 
 package club.devcord.devmarkt.auth;
 
-import club.devcord.devmarkt.entities.auth.Operation;
-import club.devcord.devmarkt.entities.auth.Permission;
 import club.devcord.devmarkt.services.PermissionService;
 import graphql.GraphQL;
-import graphql.schema.GraphQLEnumType;
-import graphql.schema.GraphQLFieldDefinition;
-import graphql.schema.GraphQLInterfaceType;
-import graphql.schema.GraphQLList;
-import graphql.schema.GraphQLNamedSchemaElement;
-import graphql.schema.GraphQLNonNull;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLScalarType;
-import graphql.schema.GraphQLSchemaElement;
-import graphql.schema.GraphQLUnionType;
 import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.context.event.StartupEvent;
-import io.micronaut.http.server.exceptions.InternalServerException;
 import jakarta.inject.Singleton;
-import java.util.HashSet;
-import java.util.stream.Stream;
 
 @Singleton
 public class PermissionUpdater implements ApplicationEventListener<StartupEvent> {
@@ -44,64 +29,18 @@ public class PermissionUpdater implements ApplicationEventListener<StartupEvent>
 
   private final PermissionService service;
   private final GraphQL graphQL;
+  private final SchemaPermissionGenerator schemaPermissionGenerator;
 
-  public PermissionUpdater(PermissionService service, GraphQL graphQL) {
+  public PermissionUpdater(PermissionService service, GraphQL graphQL,
+      SchemaPermissionGenerator schemaPermissionGenerator) {
     this.service = service;
     this.graphQL = graphQL;
+    this.schemaPermissionGenerator = schemaPermissionGenerator;
   }
 
   @Override
   public void onApplicationEvent(StartupEvent event) {
-    var schema = graphQL.getGraphQLSchema();
-    var set = new HashSet<Permission>();
-    addPermissions(set, schema.getQueryType(), Operation.QUERY);
-    addPermissions(set, schema.getMutationType(), Operation.MUTATION);
-    addPermissions(set, schema.getSubscriptionType(), Operation.SUBSCRIPTION);
-    service.updatePermissions(set);
-  }
-
-  private void addPermissions(HashSet<Permission> set, GraphQLObjectType type,
-      Operation operation) {
-    if (type == null) {
-      return;
-    }
-    type.getFieldDefinitions()
-        .stream()
-        .flatMap(fieldDefinition -> addLevel(fieldDefinition.getChildren().get(0),
-            fieldDefinition.getName(), false))
-        .forEach(s -> set.add(new Permission(-1, operation, s)));
-  }
-
-  private Stream<String> addLevel(GraphQLSchemaElement definition, String perm,
-      boolean includeName) {
-    if (definition instanceof GraphQLNonNull nonNull) {
-      return addLevel(nonNull.getWrappedType(), perm, false);
-    }
-    if (definition instanceof GraphQLScalarType || definition instanceof GraphQLEnumType) {
-      return Stream.of(perm);
-    }
-    if (definition instanceof GraphQLInterfaceType) {
-      return Stream.of();
-    }
-    if (definition instanceof GraphQLFieldDefinition type) {
-      return addChildren(type, perm + PERMISSION_SEPARATOR + type.getName(), false);
-    }
-    if (definition instanceof GraphQLNamedSchemaElement element) {
-      perm += includeName
-          ? PERMISSION_SEPARATOR + element.getName()
-          : "";
-      return addChildren(element, perm, element instanceof GraphQLUnionType);
-    }
-    if (definition instanceof GraphQLList) {
-      return addChildren(definition, perm, false);
-    }
-    throw new InternalServerException("You're an idiot :3");
-  }
-
-  private Stream<String> addChildren(GraphQLSchemaElement schemaElement, String perm,
-      boolean includeName) {
-    return schemaElement.getChildren()
-        .stream()
-        .flatMap(e -> addLevel(e, perm, includeName));
+    var permission = schemaPermissionGenerator.generate(graphQL.getGraphQLSchema());
+    service.updatePermissions(permission);
   }
 }
---
+++ backend/src/main/java/club/devcord/devmarkt/auth/SchemaPermissionGenerator.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2022 Contributors to the Devmarkt-Backend project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package club.devcord.devmarkt.auth;
+
+import club.devcord.devmarkt.entities.auth.Operation;
+import club.devcord.devmarkt.entities.auth.Permission;
+import graphql.schema.GraphQLEnumType;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLImplementingType;
+import graphql.schema.GraphQLInterfaceType;
+import graphql.schema.GraphQLList;
+import graphql.schema.GraphQLNamedSchemaElement;
+import graphql.schema.GraphQLNonNull;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLScalarType;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.GraphQLSchemaElement;
+import graphql.schema.GraphQLUnionType;
+import io.micronaut.http.server.exceptions.InternalServerException;
+import jakarta.inject.Singleton;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Singleton
+public class SchemaPermissionGenerator {
+
+  public static final String PERMISSION_SEPARATOR = ".";
+
+  public Collection<Permission> generate(GraphQLSchema schema) {
+    var permissionSet = new HashSet<Permission>();
+    permissionSet.addAll(generatePermissions(schema.getQueryType(), Operation.QUERY));
+    permissionSet.addAll(generatePermissions(schema.getMutationType(), Operation.MUTATION));
+    permissionSet.addAll(generatePermissions(schema.getSubscriptionType(), Operation.QUERY));
+    return permissionSet;
+  }
+
+  private Collection<Permission> generatePermissions(GraphQLObjectType type, Operation operation) {
+    if (type == null) {
+      return Set.of();
+    }
+    return type.getFieldDefinitions()
+        .stream()
+        .flatMap(element -> addLayer(element.getChildren().get(0), element.getName(), type)) // first child is return type, we always have one here
+        .map(s -> new Permission(-1, operation, s))
+        .collect(Collectors.toSet());
+  }
+
+  private Stream<String> addLayer(GraphQLSchemaElement element, String perm, GraphQLSchemaElement parent) {
+    if (element instanceof GraphQLNonNull nonNull) {
+      return addLayer(nonNull.getWrappedType(), perm, nonNull); // unwrap single type
+    }
+    if (element instanceof GraphQLScalarType || element instanceof GraphQLEnumType) {
+      return Stream.of(perm); // finalize this permission
+    }
+    if (element instanceof GraphQLList || element instanceof GraphQLUnionType) { // unwrap multiple union and list types
+      return element.getChildren()
+          .stream()
+          .flatMap(e -> addLayer(e, perm, element));
+    }
+    if (element instanceof GraphQLObjectType objectType) {
+      return element.getChildren()
+          .stream()
+          .flatMap(e -> {
+            // Skip interface fields, they will be added as part of the interface type later
+            if (isInterfaceField(e, objectType)) return Stream.of();
+
+            // object type children include implemented interface
+            // they will be passed to the addLayer method later, but we want the interface name and not the
+            // object type name in the permission, so we need to exclude them
+            var newPerm = parent instanceof GraphQLUnionType && !(e instanceof GraphQLInterfaceType)
+                ? addPermissionNode(perm, objectType.getName())
+                : perm;
+
+            return addLayer(e, newPerm, objectType);
+          });
+    }
+    if (element instanceof GraphQLNamedSchemaElement namedElement) { // add named schema elements e.g. fields or interface with its name to the permission
+      return namedElement
+          .getChildren()
+          .stream()
+          .flatMap(e -> addLayer(e, addPermissionNode(perm, namedElement.getName()), namedElement));
+    }
+    throw new InternalServerException("You're an idiot :3");
+  }
+
+  private boolean isInterfaceField(GraphQLSchemaElement element, GraphQLNamedSchemaElement type) {
+    return type instanceof GraphQLImplementingType implementingType
+        && element instanceof GraphQLFieldDefinition named
+        && implementingType
+        .getInterfaces()
+        .stream()
+        .map(o -> (GraphQLInterfaceType) o)
+        .anyMatch(i -> i.getFieldDefinition(named.getName()) != null);
+  }
+
+  private String addPermissionNode(String perm, String node) {
+    return perm + PERMISSION_SEPARATOR + node;
+  }
+
+}
