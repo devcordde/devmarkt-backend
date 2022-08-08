cf4418bd0613ea7e3806ef23b343cb78d3a231a7
Nick Hensel <nickhensel25@icloud.com>
2022-08-08 21:30:08 +0200

    Update micronaut and use @IgnoreWhere

--- backend/src/main/java/club/devcord/devmarkt/repositories/TemplateRepo.java
+++ backend/src/main/java/club/devcord/devmarkt/repositories/TemplateRepo.java
@@ -18,10 +18,10 @@ package club.devcord.devmarkt.repositories;
 
 import club.devcord.devmarkt.entities.template.Template;
 import io.micronaut.context.annotation.Executable;
+import io.micronaut.data.annotation.IgnoreWhere;
 import io.micronaut.data.annotation.Join;
 import io.micronaut.data.annotation.Join.Type;
 import io.micronaut.data.annotation.Query;
-import io.micronaut.data.annotation.Where;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.repository.CrudRepository;
 import java.util.List;
@@ -34,7 +34,7 @@ public interface TemplateRepo extends CrudRepository<Template, Integer> {
   boolean existsByName(String name);
 
   @Override
-  @Where("true")
+  @IgnoreWhere
   @Join(value = "questions", type = Type.LEFT_FETCH)
   Optional<Template> findById(@javax.validation.constraints.NotNull Integer integer);
 
@@ -51,8 +51,7 @@ public interface TemplateRepo extends CrudRepository<Template, Integer> {
 
   List<String> findName();
 
-  @Where("true")
-  // replaced the @Where conditions of the entity, so that we're able to find soft deleted templates.
+  @IgnoreWhere
   @Join(value = "questions", type = Type.LEFT_FETCH)
   Optional<Template> findById(int id);
 }
